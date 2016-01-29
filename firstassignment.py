cc04f597c5040c25455d40e1860601b6fad98de1
cmis <cmis@cmis-hs19.(none)>
2016-01-29 10:55:55 +0700

    assingment1notfinished

---
+++ firstassignment.py
@@ -0,0 +1,13 @@
+joshuaname = "Joshua"
+joshuaageyears = 14.65 
+joshuaheightmeters = 1.8034 
+sideofsquare = 1
+widthrectangle = 4
+heghtrectangle = 3
+months = 12.0
+ageinmonths = months * joshuaageyears
+ageinmonths
+
+
+
+
