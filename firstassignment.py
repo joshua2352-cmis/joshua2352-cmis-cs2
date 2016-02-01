b031654089c612a1980dc8a68a4637832f9f045c
cmis <cmis@cmis-hs19.(none)>
2016-02-01 13:32:41 +0700

    first assignment is getting there almost done its for calculating various equations

--- firstassignment.py
+++ firstassignment.py
@@ -7,6 +7,18 @@ heghtrectangle = 3
 months = 12.0
 ageinmonths = months * joshuaageyears
 ageinmonths
+yearsold = 14.65
+averageageofdeath = 71.0
+yearsleft = averageageofdeath - yearsold
+yearsleft
+winkyface = ";)"
+tenthousandwinkface = winkyface * 10000
+feettometer = 3.28084
+joshheightfeet = joshuaheightmeters * feettometer
+joshheightfeet
+averageusboyheight = 1.6380
+heightdifferencemetous = joshuaheightmeters - averageusboyheight
+heightdifferencemetous
 
 
 
