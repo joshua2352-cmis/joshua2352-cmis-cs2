83c5139b6641a44cab7a4089c747b99f5f058fe7
cmis <cmis@cmis-hs19.(none)>
2016-02-04 10:56:00 +0700

    is finished contains values and some shorter equations in python

--- assignment1.py
+++ assignment1.py
@@ -15,7 +15,7 @@ months = 12.0
 ageinmonths = months * joshuaageyears
 #this calculates two variables by multiplying them
 ageinmonths
-#this shows the calculation 
+#this shows the answer to the calculation 
 yearsold = 14.65
 #this sets the variable as 14.65
 averageageofdeath = 71.0
@@ -33,20 +33,32 @@ feettometer = 3.28084
 joshheightfeet = joshuaheightmeters * feettometer
 #this calculates the two variables by multipying them 
 joshheightfeet
+#this shows the answer to the calculation 
 averageusboyheight = 1.6380
+#this sets the variable as 1.380
 heightdifferencemeters = joshuaheightmeters - averageusboyheight
+#this calculates 2 variables by subtracting one from another 
 heightdifferencemeters
+#this shows the answer to the equation 
 areaofsquare = sideofsquare * sideofsquare
+#this calculates 2 variables by multiplying them 
 areaofsquare
+#this shows the answer to the equation 
 oneninthofrectanglesquared = (widthrectangle * heghtrectangle) / 9
+#this calculates two variables by multiplying them then dividing them by 9 
 oneninthofrectanglesquared
+#this shows the answer to the equation 
 halfvolumeofcube = (sideofsquare * sideofsquare * sideofsquare) / 2
+#this calculates the a variable to the power of 3 then divides it by two
 halfvolumeofcube
-print "hello I am " + str(joshuaname) +"."+ "I am " +str(joshuaageyears) + " years old. I am " + str(joshuaheightmeters) + " meters tall. I have aproximatley " + str(yearsleft) + " yearsleft. I am " +str(joshheightfeet) + " feet tall."
+#this shows the answer to the equation 
 
+print "hello I am " + str(joshuaname) +"."+ "I am " +str(joshuaageyears) + " years old. I am " + str(joshuaheightmeters) + " meters tall. I have aproximatley " + str(yearsleft) + " yearsleft. I am " +str(joshheightfeet) + " feet tall."
+#for both this replace the strings with the variables assossiated with the things in parentheses
 print "my height in feet is" ,joshheightfeet, "ft.""the differnce between me and my average height in th U.S in meters is" ,heightdifferencemeters, "one ninth of the area of a rectangle I made is equal to" ,oneninthofrectanglesquared, "units squared. the area of the square i made is" ,areaofsquare, "units squared. half the volume of a cube i made is equal to" ,halfvolumeofcube, "units cubed."
 
 
 
 print tenthousandwinkface
+#prints 10000 winky faces
 
