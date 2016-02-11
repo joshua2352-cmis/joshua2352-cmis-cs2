974c13bb40649e5b4deec5da50f2d98c1f382650
cmis <cmis@cmis-hs19.(none)>
2016-02-11 13:29:27 +0700

    functions.py not done need to finish

--- Functions.py
+++ Functions.py
@@ -1,7 +1,29 @@
 def add(a, b):
-	return a = b
-    print a = 7
-    print b = 9
-      
+    return a + b
+def sub (a, b):
+    return a - b
+def mul(a, b):
+    return a * b
+def div(a, b):
+    return a / b
+ 
+a = add(9, 43)
+b = sub(9, 43)
+c = div(9, 43)
+d = mul(9, 43)
 
+
+print a 
+print b 
+print c
+print d
+
+def hours_from_seconds(seconds):
+    hours = seconds / 3600
+    return hours
+
+hours = hours_from_seconds(86400)    
+print hours
+
+    
 
