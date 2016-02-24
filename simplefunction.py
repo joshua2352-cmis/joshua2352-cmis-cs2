c9113fd89fe4c3f6be5801e315a2359767c2b40a
cmis <cmis@cmis-hs21.(none)>
2016-02-24 10:09:51 +0700

    very close to doneness

--- simplefunction.py
+++ simplefunction.py
@@ -1,9 +1,16 @@
 import math 
-def findx((x * y ) / b = a):
+def div():
     no1 = raw_input("this will find x any comments:")
-    x = raw_input("enter first numero this will be the number being divided i.e y:")
+    b = raw_input("enter first numero this will be the number being divided i.e y:")
     y = raw_input("enter second number this will be the number dividing i.e b:")
     a = raw_input("enter third number this will be what x * y /b will equal enter any number")
-    return (x * y / b) = a 
-      
+    return (int(x) * int(y) / int(b)) = a 
+    return int(a) / int(b) /
+                           /
+
+
+
+    
+
+
     
