d9cd759802d011fc2e47220e29b34b73d921750a
cmis <cmis@cmis-hs21.(none)>
2016-03-15 13:35:55 +0700

    almost

--- ONEPUNCH.py
+++ ONEPUNCH.py
@@ -1,12 +1,17 @@
+
 import math 
+import random
+minimum = raw_input ("what is the minimum number?:")
+maximum = raw_input ("what is the maximum number?:")
+print ("im thinking of a number between {} and {}").format(int(minimum) .format(int(maximum))
+    
 def oneguess(minimum,maximum):
-    minimum = raw_input ("what is the minimum number?:")
-    maximum = raw_input ("what is the maximum number?:")
-    target = random.randint .format(int(minimum) .format(int(maximum))
-    guess = raw_input ("guess a number between" .format(int(minimum) "and" .format(int(maximum))
-if guess == target guess = True
-if guess > target = False 
-if guess < target = False
+if guess == target: 
+        return "you must be psychic"
+if guess > target: 
+        return "you have failed you were over by {}"
+if guess < target:
+        return " you are a failure you are under by {}"
 
 
  
