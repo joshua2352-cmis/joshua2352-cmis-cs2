df5f01a32d86606411a250c9800161e2a799a4e6
cmis <cmis@cmis-hs11.(none)>
2016-03-24 08:40:28 +0700

    doneeee

--- ONEPUNCH.py
+++ ONEPUNCH.py
@@ -1,17 +1,54 @@
+import math
 
-import math 
 import random
-minimum = raw_input ("what is the minimum number?:")
-maximum = raw_input ("what is the maximum number?:")
-print ("im thinking of a number between {} and {}").format(int(minimum) .format(int(maximum))
-    
-def oneguess(minimum,maximum):
-if guess == target: 
-        return "you must be psychic"
-if guess > target: 
-        return "you have failed you were over by {}"
-if guess < target:
-        return " you are a failure you are under by {}"
 
 
- 
+Minimum = int(raw_input("What's the minimum number? "))
+
+
+Maximum = int(raw_input("What's the maximum number? "))
+
+
+print "I'm thinking of a number from " + str(Minimum) + " to " + str(Maximum) + "."
+
+Guess = str(raw_input("What do you think it is?: "))
+
+def CorrectORincorrect():
+
+   x = number = random.randint(Minimum, Maximum)
+
+   if int(x) == int(Guess):
+
+       print """
+
+The target was {}
+Your guess was {}
+That's correct! You must be a SAIKOU HEROOOOOOOOOO!
+
+		""".format(str(x), str(Guess))
+
+   elif str(x) > str(Guess):
+
+       tolow = int(x) - int(Guess)
+
+       print """
+
+The target was {}
+Your guess was {}
+That's under by {}
+
+		""".format(str(x), str(Guess), tolow)
+
+   elif str(x) < str(Guess):
+
+       tohigh = int(Guess) - int(x)
+
+       print """
+
+The target was {}
+Your guess was {}
+That's over by {}
+
+		""".format(str(x), str(Guess), tohigh)
+
+CorrectORincorrect()
--- simplefunction.py
+++ simplefunction.py
@@ -1,13 +1,12 @@
 import math 
 def div():
     no1 = raw_input("this will find x any comments:")
-    b = raw_input("enter first numero this will be the number being divided i.e y:")
-    y = raw_input("enter second number this will be the number dividing i.e b:")
-    a = raw_input("enter third number this will be what x * y /b will equal enter any number")
-    return (int(x) * int(y) / int(b)) = a 
-    return int(a) / int(b) /
-                           /
+    b = raw_input("enter first numero this will be the number being divided i.e b:")
+    y = raw_input("enter second number this will be the number dividing i.e y:")
+    x = raw_input("enter third number this will be what b * y /x will equal enter any number")
+    print (int(b) * int(y) / int(x))
 
+div()
 
 
     
