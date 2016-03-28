c1fdb2f50a702be630dfa7b06c2040740bead34f
cmis <cmis@cmis-hs12.(none)>
2016-03-28 10:55:24 +0700

    almost

--- simplefunction.py
+++ simplefunction.py
@@ -1,14 +1,17 @@
 import math 
 def div():
-    no1 = raw_input("this will find x any comments:")
+    no1 = raw_input("this will find the answer to a simple equation any comments:")
     b = raw_input("enter first numero this will be the number being divided i.e b:")
     y = raw_input("enter second number this will be the number dividing i.e y:")
     x = raw_input("enter third number this will be what b * y /x will equal enter any number")
-    print (int(b) * int(y) / int(x))
+    answer=(int(b) * int(y) / int(x))
+    
+    print "for b you put",str(b),"for a you put",str(y),"for x you put",str(x),"the answer is ",str(answer),
 
 div()
-
-
+def main():
+    print ""
+main()
     
 
 
