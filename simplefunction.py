95a9d713e80371f05cf8dad044665a1a3d1b027c
cmis <cmis@cmis-hs12.(none)>
2016-03-29 13:34:37 +0700

    all done

--- simplefunction.py
+++ simplefunction.py
@@ -1,16 +1,29 @@
 import math 
-def div():
+def div(no1,b,y,x):
+    answer=(float(b) * int(y) / int(x))
+    return answer
+
+def user(b,y,x,no1):
+    actualanswer = div(no1,b,y,x)
+    out = """
+    print for (b) you put {}
+    for (y) you put {}
+    for (x) you put {} 
+    the answer is {}
+    your comment was {}
+    """.format(b,y,x,actualanswer,no1)
+    return out
+
+
+def main():
     no1 = raw_input("this will find the answer to a simple equation any comments:")
     b = raw_input("enter first numero this will be the number being divided i.e b:")
     y = raw_input("enter second number this will be the number dividing i.e y:")
-    x = raw_input("enter third number this will be what b * y /x will equal enter any number")
-    answer=(int(b) * int(y) / int(x))
-    
-    print "for b you put",str(b),"for a you put",str(y),"for x you put",str(x),"the answer is ",str(answer),
+    x = raw_input("enter third number this will be what b * y /x will equal enter any number")    
+    function = div(no1,b,y,x)
+    function2 = user(b,y,x,no1)
+    print function2
 
-div()
-def main():
-    print ""
 main()
     
 
