9c3207e69e5b13e0229493b734c9daa3451eb538
cmis <cmis@cmis-hs12.(none)>
2016-03-31 08:39:38 +0700

    hate life so hard

---
+++ test.py
@@ -0,0 +1,95 @@
+#PART 1: Terminology
+#1) Give 3 examples of boolean expressions.
+#a)3==3
+#b)3>3
+#c)3<3
+#
+#2) What does 'return' do?
+#uses the given information and spits something back out 
+#
+#
+#
+#3) What are 2 ways indentation is important in python code?
+#a)it tells you where the function definition ends
+#b)It is needed to run the function or it will get indent error thing 
+#
+#
+
+#PART 2: Reading
+#Type the values for 9 of the 12 of the variables below.
+#
+#problem1_a)36
+#problem1_b)1
+#problem1_c)0
+#problem1_d)5
+#
+#problem2_a)True
+#problem2_b)True 
+#problem2_c)False
+#problem2_d)False
+#
+#problem3_a)0.3
+#problem3_b)0.5
+#problem3_c)0.5
+#problem3_d)0.5
+#
+#problem4_a)
+#problem4_b)
+#problem4_c)
+#problem4_d)
+
+
+
+import math 
+
+
+
+
+def main(a,b,c):
+    print "type in three different numbers decimals work too:"
+    a=raw_input("A:")
+    b=raw_input("B:")
+    c=raw_input("C:")
+
+    if int(a) == int(b)
+        return "you did not follow instructions"
+    
+    if int(a) == int(c)
+        return "you did not follow instructions"
+
+    if int(c) == int(b)
+        return "you did not follow instructions"
+
+    if int(c) == int(a)
+        return "you did not follow instructions"
+
+    if int(b) == int(c)
+        return "you did not follow instructions"
+
+    if int(b) == int(a)
+        return "you did not follow instructions"
+
+    if int(a) < int(b) and int(a) < int(c) return "The largest number was"int(a)"."
+
+    if int(b) < int(c) and int(a) < int(c) return "The largest number was"int(b)"."
+
+    if int(c) < int(b) and int(a) < int(c) return "The largest number was"int(c)"."
+ 
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
