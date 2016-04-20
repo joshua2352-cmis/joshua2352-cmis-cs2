3deb446930cdb12325231a9fbea4d65f5ceffb9e
cmis <cmis@cmis-hs12.(none)>
2016-04-20 08:40:29 +0700

    almost doneish

--- wakeup.py
+++ wakeup.py
@@ -3,28 +3,44 @@ import math
 def begin():
     print "This will find the average of stuuff"
 
-def userinput(n1,n2,n3,n4,n5):
+def main(n1,n2,n3,n4,n5):
     n1=raw_input("what is the 1st number 0-10:")
-    if int(n1)> 9 or int(n1)<1:
-        wrong()
+    if float(n1)> 10 or float(n1) <= 0:
+        return"that is not within range"
+    zero1()
     n2=raw_input("what is the 2nd number 0-10:")
-    if int(n2)> 9 or int(n2)< 1:
-        wrong()
+    if float(n2)> 10 or float(n2) <= 0:
+       return"that is not within range"
+    zero2()
     n3=raw_input("what is the 3rd number 0-10:")
-    if int(n3)> 9 or int(n3)< 1:
-        wrong()
+    if float(n3)> 10 or float(n3) <= 0:
+       return "that is not within range"
+    zero3()
     n4=raw_input("what is the 4th number 0-10:")
-    if int(n4)> 9 or int(n4)< 1:
-        wrong()
+    if float(n4)> 10 or float(n4) <= 0:
+        return "that is not within range"
+    zero4()
     n5=raw_input("what is the 5th number 0-10:")
-    if int(n5)> 9 or int(n5)<1:
-        wrong()
+    if float(n5)> 10 or float(n5) <= 0:
+        return "that is not within range"
+        zero5()
 
 def add(n1,n2,n3,n4,n5):
-    averageX5 = int(n1) + int(n2) + int(n3) + int(n4) + int(n5)
+    averageX5 = float(n1) + float(n2) + float(n3) + float(n4) + float(n5)
 def div(averageX5):
-    average = int(averageX5) / 5
-
+    average = float(averageX5) / 5
+def zero1():
+    n1=0
+def zero1():
+    n2=0
+def zero1():
+    n3=0
+def zero1():
+    n4=0
+def zero1():
+    n5=0
 def wrong():
     print"that is not within range"
+begin()
+
 
