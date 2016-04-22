a32a7e6e67a7d303bde3d1764165982f4e7bd9ab
cmis <cmis@cmis-hs07.(none)>
2016-04-22 13:24:52 +0700

    hate life so hard

---
+++ countDOWN.py
@@ -0,0 +1,24 @@
+def countdown(n):
+
+
+
+
+def countdown_from_to(start,stop)
+    if  start <= stop:
+        print 'Blastoff!'
+    else:
+        print n
+        countdown(n-1)
+    countdown_from_to(10,1)
+
+def main():
+    start = raw_input("put a number to count down from:")
+    stop = raw_input("put the number you want it to stop at:")
+
+main()
+:) = :)
+print :) * 10000
+
+
+
+
