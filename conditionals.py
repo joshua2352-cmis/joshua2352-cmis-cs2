9e2fdaa9e473186c5d77440b3cf7939e90b804d7
cmis <cmis@cmis-hs13.(none)>
2016-05-04 13:33:38 +0700

    almost

--- conditionals.py
+++ conditionals.py
@@ -2,6 +2,6 @@ import math
 import random 
 def cABBAGE(,fight,run,):	
 fight=raw_input("you are attacked by cute flying cabbages do you fight yes/no:")
-if yes 
-print "luck is on your side you slay the cabbages fiercly and gain 1,000,000 Baht"
-if no "you rub away like a little girl and  
+	if yes 
+		print "luck is on your side you slay the cabbages fiercly and gain 1,000,000 Baht"
+	if no "you rub away like a little girl and  
